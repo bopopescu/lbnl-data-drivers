d3f8f957ab40400460e3a1ed0babe8ab3373ade6
[akshephard] <akshephard@ucdavis.edu>
2018-09-25 11:09:30 -0700

    added additional documentation to functions

--- python/modbus/run_script.py
+++ python/modbus/run_script.py
@@ -4,7 +4,7 @@ from struct import *
 
 from pymodbus.constants import Endian
 from pymodbus.payload import BinaryPayloadDecoder
-
+#TODO make more comprehensive test script for Modbus_Driver()
 
 
 
@@ -12,22 +12,6 @@ obj = Modbus_Driver("config.yaml")
 
 
 obj.initialize_modbus()
-"""
-target_address = 0x601
-while (target_address < 1623):
-    previous_value = obj.decode_register(target_address,'16int')
-    print(previous_value)
-    add_value = 1
-    if (previous_value < 0):
-        obj.write_data(target_address,0)
-    else:
-        obj.write_data(target_address,(previous_value + add_value))
-    current_value = obj.decode_register(target_address,'16int')
-    print("The previous value at address " +str(target_address) + " was: " +str(previous_value))
-    print("The current value at address " +str(target_address) + " is: " +str(current_value))
-    target_address += 1
-"""
-#obj.write_data(0x655,0)
 
 output = obj.get_data()
 print(output)
