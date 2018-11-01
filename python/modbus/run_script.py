7d26ae07c73782a0c8352539e8ce1aeac584adb7
[akshephard] <akshephard@ucdavis.edu>
2018-11-01 11:23:34 -0700

    Added the ability to have multiple devices with the same register configuration

--- python/modbus/run_script.py
+++ python/modbus/run_script.py
@@ -15,7 +15,12 @@ obj.initialize_modbus()
 
 output = obj.get_data()
 print(output)
+out_list = obj.get_data_all_devices()
+print("\ntest of get_data_all_devices\n")
+print(out_list)
 
-
+print("\ntest of get_data() with specific unit id\n")
+output = obj.get_data(0x1)
+print(output)
 
 obj.kill_modbus()
